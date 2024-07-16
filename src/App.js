f736a7b0ab3782d36a3c0a77124ce1c4122c2b6f
Umesh-roka <umeshmagar764@gmail.com>
2024-07-16 17:09:49 +0545

    change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import RouteLayout from './ui/RouteLayout'
-import Home from './feature/Home'
 
-
-const router = createBrowserRouter([{
-
-path:'/',
-element:<RouteLayout/>,
-children:[
-{index:true, element:<Home/>},
-]}
-])
 const App = () => {
- 
-  return <RouterProvider router={router}/>
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
 export default App
