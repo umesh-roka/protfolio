3ac5f49e9ae1f21d1ccb5a29c7a84bf7c280355f
Umesh-roka <umeshmagar764@gmail.com>
2024-09-06 19:48:15 +0545

    ch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,304 @@
-import React, { useEffect, useState } from 'react'
+import { Button, Input, Option, Radio, Select } from '@material-tailwind/react'
+import { useFormik } from 'formik'
+import React from 'react'
 
 const App = () => {
-  const [count, setCount]=useState(0);
-  const[name,setName]= useState(false)
-  useEffect(()=>{
-if(count === 3){
-  setName(true)
-}
-  },[count])
+  const {handleChange,handleSubmit,values,setFieldValue}= useFormik({
+    initialValues:{
+      username:'',
+      email:'',
+      gender:'',
+      hobbies:[],
+      country:'',
+      image:'',
+      imageReview:''
+    },
+    onSubmit :(val)=>{
+      console.log(val);
+    }
+  })
   return (
     <div>
-      <h1>{count}</h1>
-      {name? <p>Hello </p>:null }
-      <button onClick={()=>setCount(count+1)}>increment</button>
+
+    <form onSubmit={handleSubmit} className='space-y-6 max-w-[400px]'>
+      <h1>Fill up your detail</h1>
+      <div>
+        <Input
+        name='username'
+        onChange={handleChange}
+        value={values.username}
+        label='Username'
+        />
+      </div>
+      <div>
+      <Input
+        name='email'
+        onChange={handleChange}
+        value={values.email}
+        label='email'
+        />
+      </div>
+<div>
+  {radCheck.map((rad,i)=>{
+    return <Radio
+    key={i}
+    name='gender'
+    label={rad.label}
+    value={rad.value}
+    color={rad.color}
+    onChange={handleChange}
+    
+    />
+  })}
+</div>
+<div>
+  <Select name='country' onChange={(e)=> setFieldValue('country',e)} label='chose your country'>
+<Option value='india'>india</Option>
+<Option value='nepal'>nepal</Option>
+  </Select>
+</div>
+<div>
+  <Input
+  onChange={(e)=>{
+    const file = e.target.files[0];
+    setFieldValue('imageReview', URL.createObjectURL(file));
+    setFieldValue('image',file
+    )
+  }}
+  name='image'
+  type='file'
+  
+  
+  
+  
+  />
+  {values.imageReview && <img src={values.imageReview} alt='pho'/>}
+  {/* {values.imageReview && <img src={values.imageReview} alt="" />} */}
+</div>
+<Button type='submit'>Submit</Button>
+    </form>
+      
     </div>
   )
 }
 
 export default App
+
+
+
+const radCheck =[
+  {
+    value:'male',
+    label:'male',
+    color:'red'
+  },
+  {
+    value:'female',
+    label:'female',
+    color:'red'
+  }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Button, Checkbox, Input, Option, Radio, Select } from '@material-tailwind/react';
+// import { useFormik } from 'formik';
+// import React, { useEffect, useState } from 'react'
+
+// const App = () => {
+
+// const{handleChange,handleSubmit,values,setFieldValue} = useFormik({
+//   initialValues:{
+//     username:'',
+//     password:'',
+//     gender:'',
+//     hobbies:[],
+//     country:'',
+//     image:'',
+//     imageReview:''
+
+//   },
+//   onSubmit:(val)=>{
+// console.log(val);
+//   }
+// })
+
+//   const [count, setCount]=useState(0);
+//   const[name,setName]= useState(false)
+//   useEffect(()=>{
+// if(count === 3){
+//   setName(true)
+// }
+//   },[count])
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       {name? <p>Hello </p>:null }
+//       <button onClick={()=>setCount(count+1)}>increment</button>
+
+// <form onSubmit={handleSubmit} className='space-y-6 max-w-[400px]'>
+// <h1>Fill up your details</h1>
+
+// <div>
+// <Input
+// name='username'
+// label='username'
+// onChange={handleChange}
+// value={values.username}
+// />
+// </div>
+// <div>
+// <Input
+// name='email'
+// label='email'
+// onChange={handleChange}
+// value={values.email}
+// />
+// </div>
+// <div>
+//   <h1>Chose your gender</h1>
+//   {gen.map((gend,i)=>{
+//     return <Radio
+//     key={i}
+//     onChange={handleChange}
+//     name="gender"
+//     label={gend.label}
+//     color={gend.color}
+//     value={gend.value}
+
+    
+//     />
+//   })}
+// </div>
+// <div>
+//   <h1>chose your hobbie</h1>
+//   {check.map((che,i)=>{
+//   return <Checkbox
+//   key={i}
+//   onChange={handleChange}
+//   name='hobbies'
+//   label={che.label}
+//   color={che.color}
+//   value={che.value}
+  
+//   />
+//   })}
+// </div>
+// <div>
+//   <Select name='counrty' onChange={(e)=>setFieldValue('country',e)}>
+//     <Option value='Nepal'>Nepal</Option>
+//     <Option value='India'>India</Option>
+//     <Option value='China'>China</Option>
+
+//   </Select>
+// </div>
+
+// <div className='space-y-2'>
+//             <h1>Select An Image</h1>
+// <Input
+// label='image file'
+// onChange={(e)=>{
+//   const file = e.target.files[0];
+//   setFieldValue('imageReview', URL.createObjectURL(file));
+//   setFieldValue('imag',file);
+// }}
+// name='image'
+// type='file'
+
+// />
+// </div>
+
+//             {/* <Input
+//               label="Image File"
+//               onChange={(e) => {
+//                 const file = e.target.files[0];
+//                 setFieldValue('imageReview', URL.createObjectURL(file))
+//                 setFieldValue('product_image', file);
+//               }}
+//               type='file'
+//               name='image'
+//               multiple
+//               accept='image/*'
+//             />
+           
+//             {values.imageReview && <img src={values.imageReview} alt="" />}
+//           </div> */}
+// <Button type='submit'>Submit</Button>
+
+// </form>
+
+
+
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// const gen=[
+//   {
+//     label:'Male',
+//     color:'red',
+//     value:'male'
+//   },
+//   {
+//     label:'Female',
+//     color:'red',
+//     value:'female'
+//   }
+// ]
+
+// const check=[
+//   {
+// label:'dance',
+// color:'black',
+// value:'dance'
+//   },
+//   {
+//     label:'sing',
+//     color:'green',
+//     value:'sing'
+//       },
+//       {
+//         label:'game',
+//         color:'yellow',
+//         value:'game'
+//           },
+// ]
--- src/feature/Projects.js
+++ src/feature/Projects.js
@@ -13,7 +13,8 @@ const Projects = () => {
     <img className=' rounded-lg pt-[10px] lg:py-[15px] h-[100px] lg:h-[150px] mx-[2px]  lg:mx-[15px] lg:rounded-xl w-[130px] lg:w-[200px]' src="https://img.rasset.ie/001b24de-500.jpg" alt=" School" />
 
     <h1 className='text-center pt-1 text-[15px] lg:text-2xl'>Project Name</h1>
-    <button className= ' mb-2 lg:mt-2 ml-[25px] lg:ml-[15px] uppercase text-[10px] lg:text-[15px] bg-blue-gray-500 px-2 rounded'>Visit website</button>
+    <NavLink to="https://pawstore-delta.vercel.app/"><button className= ' mb-2 lg:mt-2 ml-[25px] lg:ml-[15px] uppercase text-[10px] lg:text-[15px] bg-blue-gray-500 px-2 rounded'>Visit website</button>
+    </NavLink>
   </div>  
   
   <div className='bg-blue-500 uppercase rounded-lg mt-[16px] w-[134px] h-[155px] lg:rounded-3xl lg:w-[230px] lg:h-[230px] transition ease-in-out delay-150   lg:hover:-translate-y-1 lg:hover:scale-110 lg:hover:bg-orange-400 duration-300 ...'>
